e9a03e4dcf418efa783b6299628d68ee54cab97e
Linus Dehner <dev@linusdehner.at>
2023-11-05 16:38:30 +0100

    added project generated by spring initializr

---
+++ src/main/java/github/com/ldehner/microhub/MicroHubApplication.java
@@ -0,0 +1,13 @@
+package github.com.ldehner.microhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroHubApplication.class, args);
+	}
+
+}
