9b11d901923eeac9a6cc3a4ab8cf04fc04c619a8
Linus Dehner <dev@linusdehner.at>
2023-11-08 18:41:39 +0100

    added message

--- src/main/java/github/com/ldehner/microhub/error/MessageNotFoundException.java
+++ src/main/java/github/com/ldehner/microhub/error/MessageNotFoundException.java
@@ -1,2 +1,11 @@
-package github.com.ldehner.microhub.error;public class MessageNotFoundException {
+package github.com.ldehner.microhub.error;
+
+public class MessageNotFoundException extends Exception{
+    public MessageNotFoundException(String message) {
+        super(message);
+    }
+
+    public MessageNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
